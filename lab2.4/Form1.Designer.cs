27ff38d80c239ce5024c548edfa443838e6d6167
nikita.ovchinnikov <nikita.ovchinnikov@omega-r.com>
2024-10-02 00:41:33 +0300

    Добавьте файлы проекта.

---
+++ lab2.4/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace lab2._4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(360, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(378, 10);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 1;
+            this.btnAnalyze.Text = "Analyze";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 45);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(378, 39);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(464, 81);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "Form1";
+            this.Text = "File Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lab2.4/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private async void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            string filePath = txtFilePath.Text;
+
+            if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
+            {
+                MessageBox.Show("Указан не валидный путь файла.");
+                return;
+            }
+
+            string resultFilePath = filePath.Replace(".txt", "") + " result.txt";
+
+            await Task.Run(() =>
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "lab2.exe",
+                    Arguments = $"\"{filePath}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    if (process != null)
+                    {
+                        process.PriorityClass = ProcessPriorityClass.High;
+                        process.WaitForExit();
+                    }
+                }
+            });
+
+            if (File.Exists(resultFilePath))
+            {
+                string result = File.ReadAllText(resultFilePath);
+                lblResult.Text = "Самое длинное слово:\n     " + result;
+            }
+            else
+            {
+                lblResult.Text = "Result file not found.";
+            }
+        }
+    }
+}
